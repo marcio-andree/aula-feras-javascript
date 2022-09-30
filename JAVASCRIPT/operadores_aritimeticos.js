3e79d99ccf211a5eddd98c3f5a26a75c143fde8e
Marcio André <andremarcio1988@gmail.com>
2022-09-30 10:12:08 -0300

    operadores aritmeticos em javascript

---
+++ JAVASCRIPT/operadores_aritimeticos.js
@@ -0,0 +1,12 @@
+let x =  10;
+let y = 2;
+
+console.log('A soma de x + y é: ', x + y);
+console.log('A subtração de x - y é: ', x - y);
+console.log('A multiplicação de x * y é: ', x * y);
+console.log('A divisão de x / y é: ', x / y);
+
+// x++; incrementa + 1
+// x--; descrescenta - 1
+x--;
+console.log(x);
